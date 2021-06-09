0f3bf308ec67c673a2767bcefe56c79f83c63497
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2021-06-08 20:11:48 -0700

    counter functionality and style

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>We're gonna make a counter</Text>
+      <View style={styles.counterContainer}>
+        <TouchableOpacity
+          style={styles.wrapper}
+          onPress={() => setCount(count - 1)}
+        >
+          <Text style={styles.btn}>-</Text>
+        </TouchableOpacity>
+
+        <View style={styles.wrapper}>
+          <Text style={styles.number}>{count}</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.wrapper}
+          onPress={() => setCount(count + 1)}
+        >
+          <Text style={styles.btn}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => setCount(0)}>
+          <Text style={styles.reset}>Reset</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +40,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F9DF74",
+  },
+  header: {
+    fontSize: 25,
+    color: "#61210F",
+  },
+  counterContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+  },
+  btn: {
+    fontSize: 40,
+    color: "#61210F",
+  },
+  number: {
+    fontSize: 34,
+    color: "#61210F",
+  },
+
+  reset: {
+    fontSize: 20,
+    color: "#61210F",
   },
 });
